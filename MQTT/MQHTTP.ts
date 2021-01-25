4921b921267e368ba27c9a15b6b798ce87880990
IasonArgyrakis <jasonargirakis@gmail.com>
2021-01-25 02:57:35 +0200

    a[pi test wprks

--- MQTT/MQHTTP.ts
+++ MQTT/MQHTTP.ts
@@ -2,8 +2,8 @@ import * as mqtt from "mqtt";
 import * as tls from "tls"
 
 var server1 = {
-  url: "Home-server:8883",
-  opt: { username: "API+67", password: "042f", clientId: "API",protocol:"ssl"},
+  url: "mqtts://Home-server:8883",
+  opt: { username: "API+67", password: "042f", clientId: "API",rejectUnauthorized: false},
 };
 
 class MQHTTP {
@@ -15,6 +15,7 @@ class MQHTTP {
     this.broker.on("message", function (topic, message) {
         // message is Buffer
         console.log("+++>>>"+topic +": "+message.toString());
+        //if(topic==)
     
         
       })
