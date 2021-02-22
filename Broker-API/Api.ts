dd0c3dd136c0af645ff48a8a5d8a006013014eae
IasonArgyrakis <jasonargirakis@gmail.com>
2021-02-22 03:41:59 +0200

    needs function

--- Broker-API/Api.ts
+++ Broker-API/Api.ts
@@ -1,15 +1,54 @@
-import * as  cookieParser from 'cookie-parser';
-const express = require("express");
-const app = express();
-const EXport = 4300;
+let cookie = require('cookie');
+let cookieParser = require("cookie-parser");
+let express = require("express");
+let app = express();
+let EXport = 4300;
 import { iQTT } from "../MQTT/MQHTTP";
 import { tasmCo } from "./Tasmota/_tasmotaControler";
+
 const bodyParser  = require("body-parser")
 
-let tas
+
 app.get("/", (req, res) => {
   res.send("Hello World!"); 
 });
+app.use(express.json())
+app.use(cookieParser("secrter"));
+app.post("/login", function (req, res){
+   //req.body
+   interface creds{username:string,password:string}
+   let creds:creds ={username:req.body.username,password:req.body.password}
+   //user autrherication
+   if(creds.username=="j"&&creds.password=="son")
+   {
+    res.cookie('Jas', '1', { expires: new Date(Date.now() + 90000), httpOnly: true,signed:true });
+    res.redirect('/auth')
+   }
+   
+   
+});
+
+app.use(function authenticateToken(req, res, next) {
+  //neeed to hande the reqest and next() or not 
+  if(req.signedCookies.Jas)
+  {
+    console.log("if")
+    next()
+  }else {console.log("if2");res.redirect("/login")}
+
+});
+
+
+app.get("/auth", function (req, res){
+  //req.body
+  console.log(req.signedCookies);
+  interface creds{username:string,password:string}
+  let creds:creds ={username:req.body.username,password:req.body.password}
+  
+  res.send({"ok":"ok"})
+  
+});
+
 app.get("/t", (req, res) => {
   iQTT.publishTo("cmnd/json-Bedroom/POWER", "2",2);
 
--- Broker-API/middleware/authenticate_token.ts
+++ Broker-API/middleware/authenticate_token.ts
@@ -1,23 +1,19 @@
 import {jwt} from "jsonwebtoken"
 
-
 function authenticateToken(req, res, next) {
-  // Gather the jwt access token from the request header
-  const authHeader = req.headers['authorization']
-  const token = authHeader && authHeader.split(' ')[1]
-  if (token == null) return res.sendStatus(401) // if there isn't any token
+  //neeed to hande the reqest and next() or not 
+  if(req.signedCookies.Jas)
+  {
+    console.log("if")
+    next()
+  }else {res.redirect("/login")}
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET as string, (err: any, user: any) => {
-    console.log(err)
-    if (err) return res.sendStatus(403)
-    req.user = user
-    next() // pass the execution off to whatever request the client intended
-  })
 }
+
 function generateToken(req, res, next) {
   let cookie_Conf={maxAge:86400,Secure:true}
 
-  res.cookie('Tolken',,cookie_Conf)
+  res.cookie('Tolken',"",cookie_Conf)
   next()
 }
 export {authenticateToken,generateToken}
